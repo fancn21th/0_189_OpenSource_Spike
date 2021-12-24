650fde485366ffa7613983f65d7f0dcf01dc61f2
alex <fancn21th@aliyun.com>
2021-12-24 16:26:02 +0800

    intro test

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var fyjRandomNumber = require("./index.js");
+
+describe("fyjRandomNumber", function () {
+  it("should work!", function () {
+    expect(true).to.equal(true);
+  });
+});
